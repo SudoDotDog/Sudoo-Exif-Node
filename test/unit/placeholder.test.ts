2466e08f71a145cecf568a2e4785463f5c6a1067
WMXPY <wm@mengw.io>
2020-06-01 20:34:09 -0500

    add uniot tests

---
+++ test/unit/placeholder.test.ts
@@ -0,0 +1,16 @@
+/**
+ * @author WMXPY
+ * @namespace Exif_Node
+ * @description Placeholder
+ * @override Unit
+ */
+
+import { expect } from 'chai';
+
+describe('Placeholder', (): void => {
+
+    it('Placeholder', (): void => {
+
+        expect(1).to.be.equal(1);
+    });
+});
